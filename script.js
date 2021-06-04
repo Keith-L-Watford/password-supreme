1539ade486ec7b139794b01ad56da0060879c69c
Keith Watford <keith.l.watford@gmail.com>
2021-06-04 16:32:22 -0400

    with some help, figured out how to implement iteration and how to proceed from there.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var theePassword;
+var tempArray = []
 var aBoxForArrays = []
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -19,11 +21,11 @@ function generatePassword() {
 
   var userLengthChoice = prompt("Please choose a number between 8 and 128", 12)
 
-  if (userLengthChoice < 8 || userLengthChoice > 128)
-    window.alert("Please choose a number between 8 and 128, press 'Generate Password' again.")
-
-  else if (userLengthChoice > 7 && userLengthChoice < 129)
-    userUpper = confirm("Would you like to use uppercase letters?")
+  if (userLengthChoice < 8 || userLengthChoice > 128) {
+    window.alert("Please choose a number between 8 and 128.")
+    return "Press 'Generate Password' again."
+  } else(userLengthChoice > 7 && userLengthChoice < 129)
+  userUpper = confirm("Would you like to use uppercase letters?")
   console.log(userLengthChoice);
 
 
@@ -31,86 +33,62 @@ function generatePassword() {
   if (userUpper)
     Array.prototype.push.apply(aBoxForArrays, upperCaseLetters)
   console.log(aBoxForArrays);
-  userLower = confirm("Would you like to use lowercase letters?")
+  userLower = confirm("Would you like to use lowercase letters?");
+
 
   if (userLower)
     Array.prototype.push.apply(aBoxForArrays, lowerCaseLetters)
   console.log(aBoxForArrays)
   userNumber = confirm("Would you like to use numbers?")
 
+
   if (userNumber)
     Array.prototype.push.apply(aBoxForArrays, numbers)
   console.log(aBoxForArrays);
   userSpecialChar = confirm("Would you like to use special characters?")
 
+
   if (userSpecialChar)
     Array.prototype.push.apply(aBoxForArrays, specialChar)
   console.log(aBoxForArrays);
 
-if (!userUpper && !userLower && !userNumber && !userSpecialChar)
-window.alert("Must choice atleast 1 option, press 'Generate Password' again.")
+
+  if (!userUpper && !userLower && !userNumber && !userSpecialChar)
+    window.alert("Must choice atleast 1 option, press 'Generate Password' again.")
+
 
   aBoxForArrays.sort(() => Math.random() - 0.5)
 
   console.log(aBoxForArrays)
 
-  // var userLengthChoice = length
+  for (var i = 0; i < userLengthChoice; i++) {
+    var randomChar = Math.floor(Math.random() * aBoxForArrays.length)
 
-  console.log(userLengthChoice);
+   console.log(aBoxForArrays[randomChar]) 
+   tempArray.push(aBoxForArrays[randomChar])
+  }
 
-// for (var i = 0; i < length; i++) {
-  return 
-// }
+console.log(tempArray);
 
-return aBoxForArrays.userLengthChoice.length
+  theePassword = tempArray.join("")
 
+  console.log(theePassword)
+  return theePassword
+} 
 
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-// TODO how mayn prompts do i need?
-//   a prompt for legnth of password
-// 
-
-
-
-
-
-
-
-// userChoice =
-
-// userChoice = 
-
-// userChoice = 
 
 // WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+// ****THEN I select which criteria to include in the password
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
+// ***WHEN prompted for the length of the password
+// ***THEN I choose a length of at least 8 characters and no more than 128 characters
 
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// TODO I need four confirms which will be in array form "true or false"
-// = confirm""
+// ***WHEN prompted for character types to include in the password
+// ***THEN I choose lowercase, uppercase, numeric, and/or special characters
+// ***TODO I need four confirms which will be in array form "true or false"
 
-
-// WHEN I answer each promp
-// THEN my input should be validated and at least one character type should be selected
+// ***WHEN I answer each promp
+// ****THEN my input should be validated and at least one character type should be selected
 
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
@@ -128,7 +106,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
